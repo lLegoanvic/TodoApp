c41ce942af852110e45903d9aa447fd1b01dd9fc
lLegoanvic <l.legoanvic@gmail.com>
2024-03-26 09:28:11 +0100

    burn changed to burnAll + init update

--- src/Controller/BurnCardsController.php
+++ src/Controller/BurnCardsController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Constraints\Json;
 
 class BurnCardsController extends AbstractController
 {
@@ -36,9 +37,17 @@ class BurnCardsController extends AbstractController
     {
         try{
             $init = $this->init($request);
-            $picture = $init['picture'];
-            $rarity = $init['rarity'];
-            $quantity = $init['quantity'];
+            $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+            $picture = $this->pictureRepository->find($data['id']);
+            $decodedPayload = $init['decodedPayload'];
+            if(!$picture){
+                Throw new ItemNotFoundException('l\'image n\'existe pas.');
+            }
+            if($picture->getInventory()->getUserInventory()->getId() !== $decodedPayload['id']){
+                Throw new \Exception('Invalid user');
+            }
+            $rarity = $picture->getFrame()->getId();
+            $quantity = $picture->getQuantity();
             if($rarity === 1){
                 $this->manageRarities($quantity, $picture, 20);
             }
@@ -61,34 +70,34 @@ class BurnCardsController extends AbstractController
     {
         try{
             $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
-            $quantToBurn = $data['quantToBurn'];
             $init = $this->init($request);
-            $picture = $init['picture'];
-            $rarity = $init['rarity'];
-            $quantity = $init['quantity'];
-            $expGain = ($rarity ** 2) * 5;
-            $user = $picture->getInventory()->getUserInventory();
-            $level = $user->getLevel();
-            if($quantToBurn>$quantity){
-                return new JsonResponse(['error', 'pas assez de cartes à burn.']);
+            try{
+                $user = $this->userRepository->find($data['id']);
+            } catch (\Exception $e){
+                return new JsonResponse(['message', $e->getMessage()]);
             }
-            for ($i=0; $i<$quantToBurn+1; $i++){
-                if($quantity === 1){
-                    $this->entityManager->remove($picture);
-                } else{
-                    $newQuant = $quantity --;
-                    $picture->setQuantity($newQuant);
+
+            $level = $user->getLevel();
+            $inventory = $user->getInventory();
+
+            $expGain = 0;
+            foreach ($inventory->getPictures() as $picture){
+                if($picture->getQuantity()>1){
+                    $quantity = $picture->getQuantity()-1;
+                    $expGain += ((($picture->getFrame()->getId() ** 2) * 5) * $quantity);
+                    $picture->setQuantity(1);
                     $picture->setUpdatedAt(new \DateTimeImmutable());
                     $this->entityManager->persist($picture);
                 }
 
+            }
                 $newXp = $level->getActualXp() + $expGain;
                 $level->setActualXp($newXp);
                 $level->setUpdatedAt(new \DateTimeImmutable());
                 $this->entityManager->persist($level);
                 $this->entityManager->flush();
 
-            }
+
             return new JsonResponse(['message', 'cartes burnt avec succès !']);
 
 
@@ -102,7 +111,7 @@ class BurnCardsController extends AbstractController
     }
 
 
-    private function init(Request $request): JsonResponse|array
+    private function init(Request $request): array
     {
         $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
         $picture = $this->pictureRepository->find($data['id']);
@@ -111,46 +120,66 @@ class BurnCardsController extends AbstractController
         $tokenParts = explode(".", $token);
         $tokenPayload = base64_decode($tokenParts[1]);
         $decodedPayload = json_decode($tokenPayload, true);
-        if(!$picture){
-            Throw new ItemNotFoundException('l\'image n\'existe pas.');
-        }
-        if($picture->getInventory()->getUserInventory()->getId() !== $decodedPayload['id']){
-            Throw new \Exception('Invalid user');
-        }
 
-        $rarity = $picture->getFrame()->getId();
-        $quantity = $picture->getQuantity();
-        return ['picture' => $picture, 'quantity' => $quantity, 'rarity' => $rarity];
+        return ['decodedPayload' => $decodedPayload];
     }
 
 
     private function manageRarities(int $quantity, Picture $picture, int $minus): JsonResponse
     {
-        if ($quantity > $minus) {
-            $betterPicture = $this->pictureRepository->findOneBy(['frame' => $picture->getFrame()->getId() + 1, 'pkmpicture'=>$picture->getPkmpicture(), 'inventory'=>$picture->getInventory()]);
-            if ($betterPicture) {
-                $betterPictureNewQuant = $betterPicture->getQuantity() + 1;
-                $betterPicture->setQuantity($betterPictureNewQuant);
-                $pictureNewQuant = $quantity - $minus;
-                $picture->setQuantity($pictureNewQuant);
-                $this->entityManager->persist($betterPicture);
-            } else{
-                $newPicture = new Picture();
-                $newPicture->setPkmpicture($picture->getPkmpicture());
-                $newPicture->setQuantity('1');
-                $newPicture->setFrame($this->frameRepository->find($picture->getFrame()->getId() + 1));
-                $newPicture->setCreatedAt(new \DateTimeImmutable());
-                $newPicture->setInventory($picture->getInventory());
-                $pictureNewQuant = $quantity - $minus;
-                $picture->setQuantity($pictureNewQuant);
-                $this->entityManager->persist($newPicture);
+        try {
+            $betterPicture = $this->pictureRepository->findOneBy(['frame' => $picture->getFrame()->getId() + 1, 'pkmpicture' => $picture->getPkmpicture(), 'inventory' => $picture->getInventory()]);
+
+            if ($quantity > $minus) {
+
+                if ($betterPicture) {
+                    $betterPictureNewQuant = $betterPicture->getQuantity() + 1;
+                    $betterPicture->setQuantity($betterPictureNewQuant);
+                    $pictureNewQuant = $quantity - $minus;
+                    $picture->setQuantity($pictureNewQuant);
+                    $this->entityManager->persist($betterPicture);
+                } else {
+                    $newPicture = new Picture();
+                    $newPicture->setPkmpicture($picture->getPkmpicture());
+                    $newPicture->setQuantity('1');
+                    $newPicture->setFrame($this->frameRepository->find($picture->getFrame()->getId() + 1));
+                    $newPicture->setCreatedAt(new \DateTimeImmutable());
+                    $newPicture->setInventory($picture->getInventory());
+                    $newPicture->setPkmId($picture->getPkmId());
+                    $newPicture->setPkmName($picture->getPkmName());
+                    $pictureNewQuant = $quantity - $minus;
+                    $picture->setQuantity($pictureNewQuant);
+                    $this->entityManager->persist($newPicture);
+                }
+                $picture->setUpdatedAt(new \DateTimeImmutable());
+                $this->entityManager->persist($picture);
+                $this->entityManager->flush();
+            } elseif ($quantity == $minus) {
+                if ($betterPicture) {
+                    $betterPictureNewQuant = $betterPicture->getQuantity() + 1;
+                    $betterPicture->setQuantity($betterPictureNewQuant);
+                    $this->entityManager->remove($picture);
+                    $this->entityManager->persist($betterPicture);
+                } else {
+                    $newPicture = new Picture();
+                    $newPicture->setPkmpicture($picture->getPkmpicture());
+                    $newPicture->setQuantity('1');
+                    $newPicture->setFrame($this->frameRepository->find($picture->getFrame()->getId() + 1));
+                    $newPicture->setCreatedAt(new \DateTimeImmutable());
+                    $newPicture->setInventory($picture->getInventory());
+                    $newPicture->setPkmId($picture->getPkmId());
+                    $newPicture->setPkmName($picture->getPkmName());
+                    $this->entityManager->remove($picture);
+                    $this->entityManager->persist($newPicture);
+                }
+                $this->entityManager->flush();
             }
-            $picture->setUpdatedAt(new \DateTimeImmutable());
-            $this->entityManager->persist($picture);
-            $this->entityManager->flush();
+            return new JsonResponse(['message', 'success']);
+        }catch (\Exception $e){
+            return new JsonResponse(['message', 'pas assez de carte pour fusionner']);
         }
 
-        Throw new \Exception('pas assez de cartes à fusionner.');
+
 
     }
 
